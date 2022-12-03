0d51eedb43df14069cab89c79f32013e64239348
ahmadmm01 <94947950+ahmadmm01@users.noreply.github.com>
2022-12-03 23:57:20 +0700

    add class to handle the exceptions globally

---
+++ src/main/java/exception/ProductExceptionController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ *
+ * @author MADD
+ */
+@ControllerAdvice
+public class ProductExceptionController
+{
+    
+}
